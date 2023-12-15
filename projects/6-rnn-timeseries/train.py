59bf2354ec12ac26cbcd5a1bdfffd4c7914dbe47
Hugo <hugo@MacBooks-MacBook-Pro.local>
2023-12-15 14:37:46 +0000

    Completed project 6

--- projects/6-rnn-timeseries/train.py
+++ projects/6-rnn-timeseries/train.py
@@ -4,9 +4,9 @@
 # file.  This is the minimum temperature in Melbourne over a period of 10 years.
 # You can find more data at https://github.com/jbrownlee/Datasets.
 #
-# This model with a SimpleRNN gets mean absolue error (mae) of around 9-10 degrees C.
+# This model with a SimpleRNN gets mean absolute error (mae) of around 9-10 degrees C.
 #
-# Can you get the average absolue error to below 1.85 degrees C?
+# Can you get the average absolute error to below 1.85 degrees C?
 
 
 import numpy as np
@@ -14,8 +14,8 @@ import pandas as pd
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
-from keras.layers import CuDNNLSTM, LSTM, SimpleRNN, Dropout
-from keras.callbacks import LambdaCallback
+from keras.layers import LSTM, SimpleRNN, Dropout
+from keras.callbacks import LambdaCallback, EarlyStopping
 
 import wandb
 from wandb.keras import WandbCallback
@@ -36,7 +36,7 @@ config.look_back = 20
 df = pd.read_csv('daily-min-temperatures.csv')
 data = df.Temp.astype('float32').values
 
-# convert an array of values into a dataset matrix
+# Convert an array of values into a dataset matrix
 def create_dataset(dataset):
     dataX, dataY = [], []
     for i in range(len(dataset)-config.look_back-1):
@@ -45,30 +45,31 @@ def create_dataset(dataset):
         dataY.append(dataset[i + config.look_back])
     return np.array(dataX), np.array(dataY)
     
-# normalize data to between 0 and 1
-#max_val = max(data)
-#min_val = min(data)
-#data=(data-min_val)/(max_val-min_val)
+# # Normalize data to between 0 and 1
+# max_val = max(data)
+# min_val = min(data)
+# data=(data-min_val)/(max_val-min_val)
 
-# split into train and test sets
+# Split into train and test sets
 split = int(len(data) * 0.70)
 train = data[:split]
 test = data[split:]
 
+# Crate datasets
 trainX, trainY = create_dataset(train)
 testX, testY = create_dataset(test)
 
+# Reshape to correct dimensions
 trainX = trainX[:, :, np.newaxis]
 testX = testX[:, :, np.newaxis]
 
-# create and fit the RNN
+# Create and fit the RNN
 model = Sequential()
-model.add(SimpleRNN(1, input_shape=(config.look_back,1 )))
+model.add(SimpleRNN(20, return_sequences=True, input_shape=(config.look_back, 1)))
+model.add(Dropout(0.2))
+model.add(SimpleRNN(20))
+model.add(Dense(1, activation='relu'))
 
+# early_stopping = EarlyStopping(monitor='val_loss', patience=5, min_delta=0.0001, verbose=True)
 model.compile(loss='mae', optimizer='adam', metrics=['mae'])
-model.fit(trainX, trainY, epochs=1000, batch_size=1, validation_data=(testX, testY),  callbacks=[WandbCallback(), PlotCallback(trainX, trainY, testX, testY, config.look_back, config.repeated_predictions)])
-
-
-
-
-
+model.fit(trainX, trainY, epochs=10, batch_size=1, validation_data=(testX, testY), callbacks=[WandbCallback(), PlotCallback(trainX, trainY, testX, testY, config.look_back, config.repeated_predictions)])
